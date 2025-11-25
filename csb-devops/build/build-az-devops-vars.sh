43913ff9aa6ad336fadabd3c6ab121952a2cffbc
Hrithvik S <hrithsnair90@gmail.com>
2025-11-25 14:22:51 +0800

    Add build scripts and terraform configuration for DevOps components

---
+++ csb-devops/build/build-az-devops-vars.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Placeholder to define the required environment variables for build script
+
+# Default vars
+export AZ_ORG=
+export AZ_POOL=
+export AZ_RM_SERVICE_CONN=
+
+# Variables for "az devops cli"
+export AZ_DEVOPS_SP_ID=
+export AZ_DEVOPS_SUBSCR_ID=
+export AZ_DEVOPS_SUBSCR_NAME=
+export AZ_DEVOPS_TENANT_ID=
+export AZURE_DEVOPS_EXT_AZURE_RM_SERVICE_PRINCIPAL_KEY=$AZ_DEVOPS_CLIENT_SECRET
+export AZURE_DEVOPS_EXT_PAT=
